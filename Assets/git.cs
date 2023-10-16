731719f6f3debf693e7d8fb059f0f09f77f8bfc8
okmrng <k022g1055@g.neec.ac.jp>
2023-10-16 14:59:29 +0900

    テスト

--- Assets/git.cs
+++ Assets/git.cs
@@ -7,12 +7,12 @@ public class git : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("aaaaa");
     }
 }

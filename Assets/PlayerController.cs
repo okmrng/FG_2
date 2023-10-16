afb090f95dd950c90c96bcad7de760f26565dfa3
okmrng <k022g1055@g.neec.ac.jp>
2023-10-16 23:55:40 +0900

    ジャンプと移動

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;  // 移動速度
+    public float jumpForce = 10.0f; // ジャンプの力
     Rigidbody2D ridid2d;
-    public float jumpForce = 500.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        ridid2d = GetComponent<Rigidbody2D>(); 
+        this.ridid2d = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 左右の移動
+        float horizontalInput = Input.GetAxis("Horizontal");
+        ridid2d.velocity = new Vector2(horizontalInput * moveSpeed, ridid2d.velocity.y);
+
+        // ジャンプ
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ridid2d.AddForce(transform.up * jumpForce);
+            ridid2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }

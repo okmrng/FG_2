c8fd60d22ca04f174594cd946a9e9077975fd5b1
okmrng <k022g1055@g.neec.ac.jp>
2023-10-26 10:47:28 +0900

    アビリティのアタック効果オブジェクトが画面外に出たら解放する

--- Assets/Scripts/AbilityAttckContoller.cs
+++ Assets/Scripts/AbilityAttckContoller.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AbilityAttckContoller : MonoBehaviour
 {
     GameObject player; // 自機
-    public float speed = 3;
+
+    public float speed = 3; // 速度
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,11 @@ public class AbilityAttckContoller : MonoBehaviour
     void Update()
     {
         transform.Translate(speed, 0, 0);
+    }
+
+    // 画面外に出たら解放
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
     }
 }

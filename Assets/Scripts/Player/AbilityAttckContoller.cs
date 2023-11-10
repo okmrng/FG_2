fee45b71b85173a131f719cba4a150e15d08e9ab
okmrng <k022g1055@g.neec.ac.jp>
2023-11-10 09:52:47 +0900

    AbilityAttckContollerスクリプトのコード整理

--- Assets/Scripts/Player/AbilityAttckContoller.cs
+++ Assets/Scripts/Player/AbilityAttckContoller.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class AbilityAttckContoller : MonoBehaviour
 {
-    GameObject player;                          // ���@
-    PlayerController playerScript;              // ���@�X�N���v�g
-    public GameObject abilityAttackRangePrefab; // �U���͈�
+    // ゲームオブジェクト
+    /// <summary> 自機オブジェクト </summary>
+    GameObject player;
+    /// <summary> アビリティ(攻撃)の範囲オブジェクト </summary>
+    public GameObject abilityAttackRangePrefab;
 
+    // スクリプト
+    /// <summary> 自機スクリプト </summary>
+    PlayerController playerScript;
+
+    /// <summary> 速度の設定 </summary>
     public float speedStatus = 3;
-    float speed = 0; // ���x
+    /// <summary> 速度 </summary>
+    float speed = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -37,13 +45,13 @@ public class AbilityAttckContoller : MonoBehaviour
         }
     }
 
-    // �����Œ�
+    // 等速更新
     private void FixedUpdate()
     {
         transform.Translate(speed, 0, 0);
     }
 
-    // ��ʊO�ɏo������
+    // 解放
     void OnBecameInvisible()
     {
         Destroy(this.gameObject);
